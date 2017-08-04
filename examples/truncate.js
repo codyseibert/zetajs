176433e1a9a97ade2c9fcf71a6fbfeab8e539404
Cody Seibert <cseibert113@gmail.com>
2017-08-04 17:35:30 -0400

    fixing case stuf

--- examples/truncate.js
+++ examples/truncate.js
@@ -14,36 +14,13 @@
 // truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length);
 
 let Z = require('../index');
-console.log('hi');
-debugger;
-Z(-1)
-  .case()
-    .when(n => n > 0)
-      .tap(n => console.log('-------'))
-      .tap(n => console.log('-------'))
-      .tap(n => console.log('-------'))
-      .tap(n => console.log('WTF'))
-      .transform(n => 'when 1')
-      .break()
-    .when(n => n < 0)
-      .tap(n => console.log('-------'))
-      .tap(n => console.log('-------'))
-      .tap(n => console.log('-------'))
-      .tap(n => console.log('GG'))
-      .transform(n => 'when 2')
-      .break()
-    .default()
-      .transform(n => 'default')
-      .break()
-  .close()
-  .log()
 
-// Z(["A-tisket a-tasket A green and yellow basket", 200])
-//   .case(([w, l]) => l > w.length)
-//     .first()
-//     .break()
-//   .default()
-//     .replace(0, ([w, l]) => w.substring(0, l))
-//     .map([
-//       ([w, l]) => [w.substring(0, l), l]
-//     ])
+Z(["A-tisket a-tasket A green and yellow basket", 200])
+  .case(([w, l]) => l > w.length)
+    .first()
+    .break()
+  .default()
+    .replace(0, ([w, l]) => w.substring(0, l))
+    .map([
+      ([w, l]) => [w.substring(0, l), l]
+    ])
--- index.js
+++ index.js
@@ -1,31 +1,67 @@
 const Z = function (input) { 
-  function ZI (value) {
-    return {
+  function ZI (value, parent) {
+    const ret = {
       value: value,
-      parent: null,
+      parent: parent,
+      caseValue: null,
+      setCaseValueOnBreak: false,
+      skipUntilClose: false,
+      skipUntilBreak: false,
       map (fun) {
-        return Z(value.map(fun));
+        return ZI(value.map(fun), parent);
       },
       tap (fun) {
         fun(value);
-        return Z(value);;
+        return ZI(value, parent);
       },
       filter (fun) {
-        return Z(value.filter(fun));
-      },
-      fork (label) {
-        const z = Z(this.value);
-        z.parent = this;
-        return z;
-      },
-      converge (labels, fun) {
-        return Z(fun())
+        return ZI(value.filter(fun), parent);
       },
       case () {
-        this.inCase = true;
-        return Z(value);
+        return ZI(value, this);
+      },
+      when (fun) {
+        if (!parent.caseValue && fun(value)) {
+          parent.setCaseValueOnBreak = true
+        } else {
+          parent.skipUntilBreak = true;
+        }
+        return ZI(value, parent);
+      },
+      default () {
+        return this.when(x => true);
+      },
+      break () {
+        if (parent.setCaseValueOnBreak) {
+          parent.caseValue = value;
+          parent.skipUntilClose = true;
+        }
+        parent.setCaseValueOnBreak = false;
+        parent.skipUntilBreak = false;
+        return ZI(value, parent);
+      },
+      close () {
+        parent.skipUntilClose = false;
+        return ZI(value, parent.parent);
+      },
+      transform (fun) {
+        return ZI(fun(value), parent);
+      },
+      log () {
+        return this.tap(console.log);
       }
-    } 
+    }
+
+    if (parent && (parent.skipUntilClose || parent.skipUntilBreak)) {
+      const keys = Object.keys(ret);
+      keys.splice(keys.indexOf('close'), 1);
+      keys.splice(keys.indexOf('break'), 1);
+      for (let key of keys) {
+        ret[key] = () => ZI(value, parent);
+      }
+    }
+
+    return ret;
   }
 
   return ZI(input);
